6c4d64ccf369626cfd62187a1712be21add169cb
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-02 12:35:40 +0530

    modified card tab using vuetify and add pages and update router

--- src/components/CardTab.vue
+++ src/components/CardTab.vue
@@ -1,16 +1,11 @@
 <template>
-  <section class="classes-tab">
-    <h2 class="text-white font-weight-bold text-h5">Classes</h2>
-    <div class="d-flex ga-10">
-      <a
-        href=""
-        v-for="cname in classNames"
-        :key="cname"
-        class="text-white text-subtitle-1 pa-4 font-weight-bold"
-        >{{ cname }}</a
-      >
-    </div>
-  </section>
+  <v-card class="pa-4" permanent>
+    <v-title class="text-h5 font-weight-bold">Classes</v-title>
+    <v-tabs>
+      <v-tab v-for="cname in classNames" :key="cname" :text="cname"></v-tab>
+    </v-tabs>
+    <slot></slot>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
@@ -18,10 +13,9 @@ const classNames: string[] = ["Class A", "Class B", "Class C"];
 </script>
 
 <style scoped>
-.classes-tab {
-  position: fixed;
-  width: 100%;
+.v-card :deep() {
   background-color: #341539;
-  padding: 16px 16px 0px 16px;
+  color: #fff;
+  border-radius: 0;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import homepage from "@/pages/HomePage.vue";
+import classPage from "@/pages/ClassPage.vue";
+import StudentsPage from "@/pages/StudentsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: homepage,
+    },
+    {
+      path: "/class",
+      name: "class",
+      component: classPage,
+    },
+    {
+      path: "/students",
+      name: "students",
+      component: StudentsPage,
     },
   ],
 });
