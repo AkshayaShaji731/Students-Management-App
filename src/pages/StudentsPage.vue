9971897e2f632f8833595a58ebe3d83aad74df4a
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-03 18:20:09 +0530

    removed v-card and todo-comment

--- src/pages/StudentsPage.vue
+++ src/pages/StudentsPage.vue
@@ -6,29 +6,7 @@
       </h2>
     </div>
     <div class="student-con">
-      <v-card
-        v-for="student in studentStore.studentsDetails"
-        class="font-weight-bold text-body-1 d-flex flex-column ma-4 pa-4"
-      >
-        <v-title>Student Name : {{ student.name }}</v-title>
-        <v-title>Student ID : {{ student.id }}</v-title>
-        <v-title>Class ID: {{ student.classId }}</v-title>
-        <v-title class="d-flex justify-center align-center mb-5">Marks</v-title>
-        <v-table theme="dark">
-          <thead>
-            <tr>
-              <th class="text-left">Subject</th>
-              <th class="text-left">Mark</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="mark in student.marks" :key="mark.subject">
-              <td>{{ mark.subject }}</td>
-              <td>{{ mark.mark }}</td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-card>
+      <!-- TODO:Add student card component -->
     </div>
   </section>
 </template>
@@ -61,11 +39,6 @@ onMounted(async () => {
   grid-template-columns: repeat(1, 1fr);
   background-color: #341539;
   padding: 20px 10px;
-}
-
-.v-card :deep() {
-  background-color: #481d50;
-  color: #fff;
 }
 
 @media screen and (min-width: 1024px) {
