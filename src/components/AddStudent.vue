30c975dc784578d507b5f5d26aa22946c2a01a3b
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-05 10:17:02 +0530

    created resuable form component

---
+++ src/components/AddStudent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-text-field label="Student Name"></v-text-field>
+      <v-text-field label="Student ID"></v-text-field>
+      <v-select label="Class ID" :items="classId"></v-select>
+      <v-label class="pb-4">Enter the Marks of Subjects</v-label>
+      <v-text-field label="English"></v-text-field>
+      <v-text-field label="Maths"></v-text-field>
+      <v-text-field label="Physics"></v-text-field>
+      <v-text-field label="Chemistry"></v-text-field>
+      <v-text-field label="Computer"></v-text-field>
+      <v-btn type="submit" color="primary">Submit</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+const classId: string[] = ["c1", "c2", "c3"];
+const subjects: string[] = [
+  "English",
+  "Maths",
+  "Physics",
+  "Chemistry",
+  "Computer",
+];
+</script>
+
+<style scoped>
+.v-container :deep() {
+  background-color: #343434;
+  color: #fff;
+}
+</style>
