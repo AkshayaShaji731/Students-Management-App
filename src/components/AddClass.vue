74454be8a0a40ec7ecd49fb6f6168d6975b49a94
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-08 16:51:09 +0530

    created Ui of Add class component

---
+++ src/components/AddClass.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="pa-5 h-screen position-absolute top-0 right-0 w-25">
+    <v-text-field label="Class ID"></v-text-field>
+    <v-text-field label="Class Name"></v-text-field>
+    <v-text-field label="Teacher Name"></v-text-field>
+    <v-text-field label="Total Students"></v-text-field>
+  </v-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.v-card :deep() {
+  background-color: #341539;
+  color: #fff;
+}
+</style>
