29023df41d3b32597532819f6d91bc251fdd6ffb
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-09 15:35:00 +0530

    implement AddClass functionality

--- src/components/AddClass.vue
+++ src/components/AddClass.vue
@@ -1,17 +1,40 @@
 <template>
   <v-card class="pa-5 h-screen position-absolute top-0 right-0 w-25">
-    <v-text-field label="Class ID"></v-text-field>
-    <v-text-field label="Class Name"></v-text-field>
-    <v-text-field label="Teacher Name"></v-text-field>
-    <v-text-field label="Total Students"></v-text-field>
+    <v-text-field label="Class ID" v-model="classForm.classId"></v-text-field>
+    <v-text-field
+      label="Class Name"
+      v-model="classForm.className"
+    ></v-text-field>
+    <v-text-field
+      label="Teacher Name"
+      v-model="classForm.teacherName"
+    ></v-text-field>
+    <v-text-field
+      label="Total Students"
+      v-model="classForm.totalStudents"
+    ></v-text-field>
     <v-container class="d-flex justify-space-between">
-      <v-btn>Add Student</v-btn>
+      <v-btn @click="AddClassStore.AddClass(classForm)">Add Student</v-btn>
       <v-btn>Cancel</v-btn>
     </v-container>
   </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, reactive } from "vue";
+
+import { useStudentStore } from "@/store/StudentStore";
+import type { ClassDetails } from "@/types/ClassDetailType";
+
+const AddClassStore = useStudentStore();
+
+const classForm = reactive<ClassDetails>({
+  classId: "",
+  className: "",
+  teacherName: "",
+  totalStudents: 0,
+});
+</script>
 
 <style scoped>
 .v-card :deep() {
