002f8099e375be6118a054c904e63b8c62a4c3f9
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-09 12:24:54 +0530

    Added btn conatiner

--- src/components/AddClass.vue
+++ src/components/AddClass.vue
@@ -4,6 +4,10 @@
     <v-text-field label="Class Name"></v-text-field>
     <v-text-field label="Teacher Name"></v-text-field>
     <v-text-field label="Total Students"></v-text-field>
+    <v-container class="d-flex justify-space-between">
+      <v-btn>Add Student</v-btn>
+      <v-btn>Cancel</v-btn>
+    </v-container>
   </v-card>
 </template>
 
