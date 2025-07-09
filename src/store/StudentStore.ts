49d95baafaf293ba06294fc1fecaddc6d8e07e15
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-09 15:35:00 +0530

    implement AddClass functionality

---
+++ src/store/StudentStore.ts
@@ -0,0 +1,27 @@
+import { ref } from "vue";
+import axios from "axios";
+import { defineStore } from "pinia";
+
+import type { ClassDetails } from "@/types/ClassDetailType";
+import { BASE_API_URL } from "@/constant";
+
+export const useStudentStore = defineStore(
+  "studentStore",
+  () => {
+    const AddClass = async (classes: ClassDetails): Promise<void> => {
+      try {
+        const res = await axios.post(`${BASE_API_URL}/classes`, classes);
+        classes.classId = "";
+        classes.className = "";
+        classes.teacherName = "";
+        classes.totalStudents = 0;
+      } catch (error) {
+        console.error("Error fetching job", error);
+      }
+    };
+    return { AddClass };
+  },
+  {
+    persist: true,
+  }
+);
