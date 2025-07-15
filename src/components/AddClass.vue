d4b3c9afaf491a3833a7e34e233b764c9dcede87
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-15 18:13:37 +0530

    made responsive addclass form

--- src/components/AddClass.vue
+++ src/components/AddClass.vue
@@ -1,13 +1,13 @@
 <template>
   <v-form
-    class="pa-5 h-screen position-absolute top-0 right-0 w-25"
+    class="pa-5 h-screen position-absolute top-0 right-0"
     v-if="props.addClassActive"
   >
     <v-label class="text-h5 pa-2 font-weight-bold">Create New class</v-label>
     <v-text-field label="Class ID" v-model="classForm.classId" />
     <v-text-field label="Class Name" v-model="classForm.className" />
     <v-text-field label="Teacher Name" v-model="classForm.teacherName" />
-    <v-container class="d-flex justify-space-between">
+    <v-container>
       <v-btn @click="addClassStore.addClass(classForm)">Add Class</v-btn>
       <v-btn @click="handleCancel()">Cancel</v-btn>
     </v-container>
@@ -43,5 +43,32 @@ const classForm = reactive<ClassDetails>({
   background-color: #330238;
   color: #fff;
   border: 1px solid #2b0c2e;
+}
+
+.v-container :deep() {
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+}
+
+@media screen and (min-width: 768px) {
+  .v-container :deep() {
+    flex-direction: row;
+  }
+  .v-form :deep() {
+    width: 75%;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .v-form :deep() {
+    width: 50%;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .v-form :deep() {
+    width: 25%;
+  }
 }
 </style>
