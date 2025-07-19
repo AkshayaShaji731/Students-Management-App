96ea746e9de863acd0610ded78458e04f32e4ebb
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-19 09:28:46 +0530

    pagination added in the students table

--- src/components/StudentTable.vue
+++ src/components/StudentTable.vue
@@ -5,7 +5,7 @@
         <th v-for="head in tableHeading">{{ head }}</th>
       </tr>
     </thead>
-    <tbody v-for="student in students" :key="student.id">
+    <tbody v-for="student in paginatedItems" :key="student.id">
       <tr>
         <td>{{ student.name }}</td>
         <td>{{ student.id }}</td>
@@ -13,17 +13,35 @@
       </tr>
     </tbody>
   </v-table>
+  <v-pagination v-model="page" :length="totalPages" rounded="0"></v-pagination>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 import { useStudentStore } from "@/stores/StudentStore";
+import type { Student } from "@/types/Students";
 
 const studentStore = useStudentStore();
-const tableHeading = ref<string[]>(["Student Name", "Student ID", "ClassName"]);
 
-defineProps({
-  students: Object,
+interface Students {
+  students: Student[];
+}
+
+const props = defineProps<Students>();
+
+const tableHeading = ref<string[]>(["Student Name", "Student ID", "ClassName"]);
+const page = ref(1);
+const pageSize = ref(10);
+const students = ref(props.students);
+
+const totalPages = computed(() => {
+  return Math.ceil(students.value.length / pageSize.value);
+});
+
+const paginatedStudents = computed(() => {
+  const start = (page.value - 1) * pageSize.value;
+  const end = start + pageSize.value;
+  return students.value.slice(start, end);
 });
 </script>
