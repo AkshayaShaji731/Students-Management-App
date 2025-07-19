05fa0189cd601d436bfd4532a625916ab6f49bec
Akshaya Shaji <akshayashaji66@gmail.com>
2025-07-19 12:51:30 +0530

    demo created

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="home-page-container">
     <h3>Student Management App</h3>
+    <sign-up-page />
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SignUpPage from "@/pages/SignUpPage.vue";
+</script>
 
 <style scoped>
 .home-page-container {
-  margin-left: 100px;
+  margin-left: 130px;
 }
 </style>
---
+++ src/pages/SignUpPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <v-card-title class="text-center">LOGIN</v-card-title>
+    <v-form class="d-flex ga-4 justify-center flex-column align-center">
+      <v-text-field label="Username"></v-text-field>
+      <v-text-field label="PassWord"></v-text-field>
+      <v-btn>LOGIN</v-btn>
+    </v-form>
+    <v-card-text class="forget-text">forget password</v-card-text>
+  </v-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+:deep.v-text-field,
+.v-btn {
+  width: 100%;
+}
+
+.forget-text {
+  position: absolute;
+  right: 0;
+}
+
+@media screen and (min-width: 768px) {
+  :deep.v-text-field,
+  .v-btn {
+    width: 75%;
+  }
+
+  .forget-text {
+    right: 10%;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  :deep.v-text-field,
+  .v-btn {
+    width: 50%;
+  }
+  .forget-text {
+    right: 25%;
+  }
+}
+@media screen and (min-width: 1440px) {
+  .forget-text {
+    right: 28%;
+  }
+}
+@media screen and (min-width: 1469px) {
+  .forget-text {
+    right: 30%;
+  }
+}
+</style>
